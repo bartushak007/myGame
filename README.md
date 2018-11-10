3b50f8338784295666472a74d15a62ac76675c01
rmnbrtshk <rmnbrtshk@gmail.com>
2018-11-10 17:50:35 +0200

    commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# myGame
-[link](https://bartushak007.github.io/myGame/app/)
--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,32 @@
 var gulp = require("gulp")
-    sass = require("gulp-sass")
-    browserSync = require("browser-sync");
+sass = require("gulp-sass")
+browserSync = require("browser-sync");
 
-    gulp.task("sass", function(){
-    	gulp.src('app/scss/main.scss')
-    	.pipe(sass())
-    	.pipe(gulp.dest('app/css'))
-    	.pipe(browserSync.reload({stream: true}));
-    })
+gulp.task("sass", function(){
+   gulp.src('app/scss/main.scss')
+   .pipe(sass())
+   .pipe(gulp.dest('app/css'))
+   .pipe(browserSync.reload({stream: true}));
+})
 
-    gulp.task("watch",["sass", "browser"], function(){
-    	gulp.watch("app/scss/**/*.scss", ['sass'])
-    })
+gulp.task("watch",["sass", "browser"], function(){
+   gulp.watch("app/scss/**/*.scss", ['sass'])
+})
 
-    gulp.task("browser", function(){
-    	browserSync({
-    		server: {baseDir:"app"},
-    		notify: false})
-    })
+gulp.task("browser", function(){
+   browserSync({
+      server: {baseDir:"app"},
+      notify: false})
+})
+
+gulp.task('build', function() {
+
+    var buildCss = gulp.src([
+        'app/css/*.css',        
+        ])
+    .pipe(gulp.dest('dist/css'))   
+
+    var buildHtml = gulp.src('app/*.html') // Переносим HTML в продакшен
+    .pipe(gulp.dest('dist'));
+
+});
